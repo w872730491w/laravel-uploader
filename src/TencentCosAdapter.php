eda6578cba578db69029ae1422417cccbbb66153
王伟懿 <872730491@qq.com>
2023-02-09 15:18:23 +0800

    wwy

---
+++ src/TencentCosAdapter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Wwy\FileSystem\Uploader;
+
+use QCloud\COSSTS\Sts;
+use Overtrue\Flysystem\Cos\CosAdapter;
+use League\Flysystem\PathPrefixer;
+
+class TencentCosAdapter extends CosAdapter
+{
+    public function getTokenConfig(string $path = '/', array $customData = [])
+    {
+        $path = (new PathPrefixer($this->config['prefix'], DIRECTORY_SEPARATOR))->prefixPath($path);
+        $config = \array_merge([
+            'url' => 'https://sts.tencentcloudapi.com/', // url和domain保持一致
+            'domain' => 'sts.tencentcloudapi.com', // 域名，非必须，默认为 sts.tencentcloudapi.com
+            'proxy' => '',
+            'secretId' => $this->config['secret_id'], // 固定密钥,若为明文密钥，请直接以'xxx'形式填入，不要填写到getenv()函数中
+            'secretKey' => $this->config['secret_key'], // 固定密钥,若为明文密钥，请直接以'xxx'形式填入，不要填写到getenv()函数中
+            'bucket' => $this->config['bucket'], // 换成你的 bucket
+            'region' => $this->config['region'], // 换成 bucket 所在园区
+            'durationSeconds' => $this->config['expire_time'], // 密钥有效期
+            'allowPrefix' => $path, // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径，例子： a.jpg 或者 a/* 或者 * (使用通配符*存在重大安全风险, 请谨慎评估使用)
+            // 密钥的权限列表。简单上传和分片需要以下的权限，其他权限列表请看 https://cloud.tencent.com/document/product/436/31923
+            'allowActions' => [
+                // 简单上传
+                'name/cos:PutObject',
+                'name/cos:PostObject',
+                // 分片上传
+                'name/cos:InitiateMultipartUpload',
+                'name/cos:ListMultipartUploads',
+                'name/cos:ListParts',
+                'name/cos:UploadPart',
+                'name/cos:CompleteMultipartUpload'
+            ]
+        ], $customData);
+
+        $tempKeys = (new Sts)->getTempKeys($config);
+
+        $res = [
+            'bucket' => $config['bucket'], // 换成你的 bucket
+            'region' => $config['region'], // 换成 bucket 所在园区
+            'domain' => $this->config['domain'],
+            'path' => $path,
+            'tempKeys'  => $tempKeys
+        ];
+
+        return $res;
+    }
+}
--- src/Uploader.php
+++ src/Uploader.php
@@ -10,13 +10,16 @@ class Uploader
      * @param array $config
      * @return mixed
      */
-    public static function getAdapter(array $config)
+    public static function getAdapter(array $baseConfig)
     {
-        $type = $config['type'];
+        $type = $baseConfig['type'];
+        $config = $baseConfig[$type];
+        $config['max_size'] = $baseConfig['max_size'];
+        $config['expire_time'] = $baseConfig['expire_time'];
 
         if ($type === 'aliyun') {
             $root = $config['prefix'] ?? null;
-            
+
             $adapter = new AliyunOssAdapter(
                 $config['access_key_id'],
                 $config['access_key_secret'],
@@ -29,6 +32,28 @@ class Uploader
                 $config['max_size'] * 1024 * 1024,
                 []
             );
+        }
+
+        if ($type === 'tencent') {
+            $config = [
+                'app_id' => $config['app_id'],
+                'secret_id' => $config['secret_id'],
+                'secret_key' => $config['secret_key'],
+                'region' => $config['region'],
+                'bucket' => $config['bucket'],
+                // 可选，如果 bucket 为私有访问请打开此项
+                'signed_url' => false,
+                // 可选，是否使用 https，默认 false
+                'use_https' => true,
+                // 可选，自定义域名
+                'domain' => $config['domain'] ?? null,
+                // 可选，使用 CDN 域名时指定生成的 URL host
+                'cdn' => $config['cdn'] ?? null,
+                'prefix' => $config['prefix'] ?? '/',
+                'expire_time' => $config['expire_time'],
+            ];
+
+            $adapter = new TencentCosAdapter($config);
         }
 
         return $adapter;
