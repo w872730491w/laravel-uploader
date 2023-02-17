83519ab4955e49973d77cbcc61d0a43737437125
王伟懿 <872730491@qq.com>
2023-02-17 10:53:03 +0800

    wwy

--- src/Controllers/Callback.php
+++ src/Controllers/Callback.php
@@ -16,7 +16,14 @@ class Callback extends Controller
         if ($config['type'] === 'aliyun') {
             $adapter = $storage->getAdapter();
             list($verify, $data) = $adapter->verify();
-
+            
+            Log::build([
+                'path' => storage_path('logs/upload/info.log'),
+            ])->error('上传回调', [
+                'auth' => $_SERVER['HTTP_AUTHORIZATION'],
+                'pub_key' => $_SERVER['HTTP_X_OSS_PUB_KEY_URL'],
+            ]);
+            
             if (!$verify) {
                 Log::build([
                     'driver' => 'single',
