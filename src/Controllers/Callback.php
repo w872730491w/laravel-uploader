8a8a92f92b00c9bcc93293a260f6eed1766aa4f4
王伟懿 <872730491@qq.com>
2023-02-17 10:12:46 +0800

    wwy

---
+++ src/Controllers/Callback.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lanyunit\FileSystem\Uploader\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class Callback extends Controller
+{
+    public function index()
+    {
+        $storage = Storage::disk();
+        $config = $storage->getConfig();
+
+        if ($config['type'] === 'aliyun') {
+            $adapter = $storage->getAdapter();
+            list($verify, $data) = $adapter->verify();
+
+            if (!$verify) {
+                Log::build([
+                    'driver' => 'single',
+                    'path' => storage_path('logs/upload/error.log'),
+                ])->error('上传回调失败', $data);
+            }
+
+            return response()->json($data);
+        }
+
+        abort(404);
+    }
+}
--- src/UploaderServiceProvider.php
+++ src/UploaderServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Lanyunit\FileSystem\Uploader;
 
 use League\Flysystem\Filesystem;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Filesystem\FilesystemAdapter;
 
@@ -13,9 +14,26 @@ class UploaderServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->registerRoutes();
+
         app('filesystem')->extend('uploader', function ($app, $config) {
             $adapter = Uploader::getAdapter($config);
             return new FilesystemAdapter(new Filesystem($adapter), $adapter, $config);
+        });
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    private function registerRoutes()
+    {
+        Route::group([
+            'namespace' => 'Lanyunit\FileSystem\Uploader\Controllers',
+            'prefix' => 'api/upload',
+        ], function () {
+            Route::post('callback', 'Callback@index');
         });
     }
 }
